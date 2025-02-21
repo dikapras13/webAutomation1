eaef30a951d296fbbdf01b4f40a7aaaa471da7a9
Dika Eko Prasetyo <dika.eko13@gmail.com>
2025-02-21 15:42:07 +0700

    Test membuat agent

--- test.js
+++ test.js
@@ -1,4 +1,11 @@
-const  {Builder, By, Key, until,} = require("selenium-webdriver");
+// Test membuat agent untuk melewati chaptcha
+
+const {
+    Builder,
+    By,
+    Key,
+    until,
+} = require("selenium-webdriver");
 // require("chromedriver");
 const chrome = require("selenium-webdriver/chrome");
 
@@ -6,17 +13,17 @@ const chrome = require("selenium-webdriver/chrome");
 async function exampleTest() {
 
     // membuat agent yang menyerupai user asli untuk melewati chaptca google
-    let options = new  chrome.Options();
+    let options = new chrome.Options();
 
     options.addArguments(
         "user-agent=Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"
-      );
+    );
     options.addArguments("--disable-blink-features=AutomationControlled"); //sembunyikan otomisasi
-    
+
 
     //membuat koneksi dengan selenium webdriver yang ada di browser
     let driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
-    try{
+    try {
 
         //buka halaman web
         await driver.get("http://www.google.com");
@@ -24,10 +31,10 @@ async function exampleTest() {
         //melakukan pencarian di google
         let searchBox = await driver.findElement(By.name("q")); // mencari element yang diinginkan
 
-       //simulasi user behavior
+        //simulasi user behavior
         await searchBox.sendKeys("Hello World", Key.RETURN);
-        await driver.wait(until.elementsLocated(By.id("result-states")),10000) // menunggu sampai elemen yang diingikan ditemukan 
-     
+        await driver.wait(until.elementsLocated(By.id("result-states")), 10000) // menunggu sampai elemen yang diingikan ditemukan 
+
         let title = await driver.getTitle()
         console.log(`Page titile is : ${title}`)
 
@@ -37,7 +44,7 @@ async function exampleTest() {
         await driver.quit();
     }
 
-    
+
 }
 
 exampleTest();
