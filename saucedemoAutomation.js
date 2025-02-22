d685395c4e497dcbda266f7d8640283752fa867e
Dika Eko Prasetyo <dika.eko13@gmail.com>
2025-02-22 14:21:29 +0700

    sucedemo

--- saucedemoAutomation.js
+++ saucedemoAutomation.js
@@ -9,9 +9,12 @@ const assert = require('assert');
 async function saucedemoLogin() {
 
     let driver = await new Builder().forBrowser("chrome").build();
+    await driver.manage().window().maximize();
     try {
+
         //mengakses website Saucedemo
         await driver.get("https://www.saucedemo.com/");
+        await driver.sleep(1000);
 
         //menginputkan username dan password
         await driver.findElement(By.id('user-name')).sendKeys('standard_user');
@@ -23,19 +26,17 @@ async function saucedemoLogin() {
         // validasi apakah sudah berhasil menampilkan halaman dashboard
         let titleText = await driver.findElement(By.css('.app_logo')).getText();
         assert.strictEqual(titleText.includes('Swag Lab'), true, "Title Does not include Swag Labs");
+        await driver.sleep(5000);
 
-        // 
-
-        await driver.sleep(5000)
-
+        // menambahkan produk ke keranjang
         await driver.findElement(By.id('add-to-cart-sauce-labs-backpack')).click();
 
+        // melakukan validasi apakah produk berhasil ditambahkan ke keranjang
         let cart = await driver.findElement(By.css('.shopping_cart_badge'));
-
         assert.strictEqual(await cart.isDisplayed(), true, "You haven't selected a product yet");
+        await driver.sleep(10000);
 
 
-        await driver.sleep(10000)
     } finally {
         await driver.quit();
     }
